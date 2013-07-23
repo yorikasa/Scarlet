ab6d814d30e6bc314b5b6df47647278c767f18d1
Yuki Orikasa <yuki@yorikasa.com>
2013-07-23 19:23:41 +0900

    Initial Commit

---
+++ Scarlet/ScarletAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  ScarletAppDelegate.h
+//  Scarlet
+//
+//  Created by Yuki Orikasa on 7/23/13.
+//  Copyright (c) 2013 Yuki Orikasa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ScarletAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ Scarlet/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Scarlet
+//
+//  Created by Yuki Orikasa on 7/23/13.
+//  Copyright (c) 2013 Yuki Orikasa. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
